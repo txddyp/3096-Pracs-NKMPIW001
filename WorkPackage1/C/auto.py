33787385e4088b61823cdf187942165c791a762d
txddy-p <piwanit@gmail.com>
2022-08-13 19:24:49 +0100

    Further changes to automation

--- WorkPackage1/C/auto.py
+++ WorkPackage1/C/auto.py
@@ -3,16 +3,19 @@ from os import system as sy
 from subprocess import Popen as op
 import tempfile as tf
 
-with tf.TemporaryFile() as tempf:
-  proc = op(['make','run_threaded'],stdout=tempf)
-  proc.wait()
-  tempf.seek(0)
-  a = tempf.read().decode("utf-8")
-  a = list(a.split('\n'))
-  tp = a[4]
-  tp = tp[32::]
+ledger = []
 
-'''
+def reader():
+  with tf.TemporaryFile() as tempf:
+    proc = op(['make','run_threaded'],stdout=tempf)
+    proc.wait()
+    tempf.seek(0)
+    a = tempf.read().decode("utf-8")
+    a = list(a.split('\n'))
+    tp = a[4]
+  return tp[32::]
+
+
 f = open('src/CHeterodyning_threaded.h','r')
 filedata = f.readlines()
 f.close()
@@ -29,14 +32,16 @@ f = open('src/CHeterodyning_threaded.h','w')
 f.write(newdata)
 f.close()
 
-
 sy('make threaded')
 sy('clear')
-'''
-'''
-for i in range(5):
-  sy('make run_threaded')
 
-print(f'{t} threads were used')
-'''
+top = f"Results obtained from multithreading using {t} threads..."
+tp = ""
+for i in range(3):
+  rp = reader()
+  tp = tp +f"\nrun {i+1} with time = " +rp
+top+=tp
+top+= '\n'
 
+with open("results.txt","a") as text_file:
+  text_file.write(top + "\n")
