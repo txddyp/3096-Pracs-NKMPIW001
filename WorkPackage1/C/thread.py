1f8ca91dcbb5d57c3015a3290ec99756281f9153
txddyp <110390129+txddyp@users.noreply.github.com>
2022-08-14 23:32:37 +0200

    Update and rename timer.py to thread.py

--- WorkPackage1/C/thread.py
+++ WorkPackage1/C/thread.py
@@ -3,16 +3,7 @@ from os import system as sy
 from subprocess import Popen as op
 import tempfile as tf
 
-with tf.TemporaryFile() as tempf:
-  proc = op(['make','run_threaded'],stdout=tempf)
-  proc.wait()
-  tempf.seek(0)
-  a = tempf.read().decode("utf-8")
-  a = list(a.split('\n'))
-  tp = a[4]
-  tp = tp[32::]
 
-'''
 f = open('src/CHeterodyning_threaded.h','r')
 filedata = f.readlines()
 f.close()
@@ -28,15 +19,3 @@ for i in filedata:
 f = open('src/CHeterodyning_threaded.h','w')
 f.write(newdata)
 f.close()
-
-
-sy('make threaded')
-sy('clear')
-'''
-'''
-for i in range(5):
-  sy('make run_threaded')
-
-print(f'{t} threads were used')
-'''
-
